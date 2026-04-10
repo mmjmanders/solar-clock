84d4b370076351cef3b68f827be37b9d3dbc34c7
Mark Manders <mmjmanders@gmail.com>
2026-04-10 10:58:20 +0200

    Feature/add queries (#4)
    * Added ipify query
    
    * Add iplocation query and integrate Zod for query schema validation
    
    * Added sunrise sunset query

---
+++ src/queries/geo.ts
@@ -0,0 +1,27 @@
+import * as z from 'zod'
+import { queryOptions, useQuery } from '@tanstack/vue-query'
+
+const { VITE_GEO_API_BASE_URL: baseUrl } = import.meta.env
+
+const geoDataSchema = z.object({
+  location: z.object({
+    timezone: z.string(),
+    coordinates: z.object({
+      latitude: z.number(),
+      longitude: z.number(),
+    }),
+  }),
+})
+
+export type GeoData = z.infer<typeof geoDataSchema>
+
+export const useGeoQuery = () =>
+  useQuery(
+    queryOptions({
+      queryKey: ['geo'],
+      queryFn: async (): Promise<GeoData> => {
+        const response = await fetch(`${baseUrl}/geo`)
+        return response.json()
+      },
+    }),
+  )
