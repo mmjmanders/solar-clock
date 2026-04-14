020801409b267c0ee5d62ae4e9582000d45de1f7
Mark Manders <mmjmanders@gmail.com>
2026-04-14 15:13:01 +0200

    Feature/add sun lib (#10)
    * Added sunrise lib
    
    * Replaced api call with library
    
    * Added shades for day / night

--- src/components/SolarClock.vue
+++ src/components/SolarClock.vue
@@ -1,19 +1,55 @@
 <script setup lang="ts">
-import { useClock, useLocation } from '@/composables'
-import { useSunriseSunsetQuery } from '@/queries'
+import { useClock, useLocation, useSunriseSunset } from '@/composables'
 import SolarNoonPosition from './SolarNoonPosition.vue'
 import { computed, readonly, ref } from 'vue'
 import SunPosition from './SunPosition.vue'
+import dayjs from 'dayjs'
 
 const { time } = useClock()
 const { latitude, longitude, location } = useLocation()
-const { data: sunriseSunsetData } = useSunriseSunsetQuery(latitude, longitude)
+const { sunrise, sunset, solarNoon } = useSunriseSunset(latitude, longitude, time)
 
 const hour = computed<number>(() => time.value.getHours())
 const minute = computed<number>(() => time.value.getMinutes())
 
 const radius = readonly(ref<number>(600))
 const pathLength = readonly(ref<number>(650))
+const outerRadius = readonly(ref<number>(800))
+
+const offset = (hour: number, minute: number): number => {
+  return ((hour * 60 + minute) / (24 * 60)) * (2 * Math.PI)
+}
+
+const offsetSunrise = computed<{ x: number; y: number } | undefined>(() => {
+  if (!sunrise.value) return undefined
+  const date = dayjs(sunrise.value)
+  const pos = offset(date.hour(), date.minute())
+  return { x: Math.cos(pos) * outerRadius.value, y: Math.sin(pos) * outerRadius.value }
+})
+
+const offsetSunset = computed<{ x: number; y: number } | undefined>(() => {
+  if (!sunset.value) return undefined
+  const date = dayjs(sunset.value)
+  const pos = offset(date.hour(), date.minute())
+  return { x: Math.cos(pos) * outerRadius.value, y: Math.sin(pos) * outerRadius.value }
+})
+
+const path = computed<string>(() =>
+  offsetSunrise.value && offsetSunset.value
+    ? [
+        'M 0 0 L',
+        offsetSunrise.value.x,
+        offsetSunrise.value.y,
+        'A',
+        outerRadius.value,
+        outerRadius.value,
+        '0 0 0',
+        offsetSunset.value.x,
+        offsetSunset.value.y,
+        'Z',
+      ].join(' ')
+    : '',
+)
 </script>
 
 <template>
@@ -21,6 +57,10 @@ const pathLength = readonly(ref<number>(650))
     <h2>{{ location }}</h2>
     <div>
       <svg viewBox="-1000 -1000 2000 2000">
+        <g v-if="path" transform="rotate(90)">
+          <circle cx="0" cy="0" :r="outerRadius" class="fill-sundial-sky-noon" />
+          <path :d="path" class="fill-sundial-sky-midnight" />
+        </g>
         <circle
           cx="0"
           cy="0"
@@ -28,15 +68,15 @@ const pathLength = readonly(ref<number>(650))
           class="stroke-sundial-bronze-300 fill-none"
           stroke-width="2"
         />
-        <template v-if="sunriseSunsetData">
-          <SolarNoonPosition :radius="radius" :solar-noon="sunriseSunsetData.solar_noon" />
+        <template v-if="sunrise && sunset && solarNoon">
+          <SolarNoonPosition :radius="radius" :solar-noon="solarNoon" />
           <SunPosition
             :radius="radius"
             :path-length="pathLength"
             :hour="hour"
             :minute="minute"
-            :sunrise="sunriseSunsetData.sunrise"
-            :sunset="sunriseSunsetData.sunset"
+            :sunrise="sunrise"
+            :sunset="sunset"
           />
         </template>
       </svg>
