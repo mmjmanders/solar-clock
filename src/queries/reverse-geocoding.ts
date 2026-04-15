dcb21ee4754f398410c2fa263028718643b51169
Mark Manders <mmjmanders@gmail.com>
2026-04-15 11:32:34 +0200

    Updated reverse geocoding api

--- src/queries/reverse-geocoding.ts
+++ src/queries/reverse-geocoding.ts
@@ -2,23 +2,19 @@ import * as z from 'zod'
 import { computed, type Ref } from 'vue'
 import { useQuery } from '@tanstack/vue-query'
 
-const { VITE_REVERSE_GEOCODING_API_BASE_URL: baseUrl, VITE_GEOAPIFY_API_KEY: apiKey } = import.meta
-  .env
+const { VITE_REVERSE_GEOCODING_API_BASE_URL: baseUrl } = import.meta.env
+
+const addressSchema = z.object({
+  city: z.string(),
+  country: z.string(),
+})
 
 const reverseGeocodingDataSchema = z.object({
-  results: z.array(
-    z.object({
-      city: z.string(),
-      country: z.string(),
-    }),
-  ),
+  address: addressSchema,
 })
 
 type ReverseGeocodingData = z.infer<typeof reverseGeocodingDataSchema>
-type LocationData = {
-  city: string
-  country: string
-}
+type AddressData = z.infer<typeof addressSchema>
 
 export const useReverseGeocodingQuery = (
   latitude: Ref<number | undefined>,
@@ -29,10 +25,10 @@ export const useReverseGeocodingQuery = (
     enabled: computed(() => latitude.value != undefined && longitude.value != undefined),
     queryFn: async (): Promise<ReverseGeocodingData> => {
       const response = await fetch(
-        `${baseUrl}/?lat=${latitude.value}&lon=${longitude.value}&apiKey=${apiKey}&format=json&limit=1`,
+        `${baseUrl}?lat=${latitude.value}&lon=${longitude.value}&layer=address&format=jsonv2`,
       )
       return response.json()
     },
-    select: (data: ReverseGeocodingData): LocationData => data.results[0]!,
+    select: (data: ReverseGeocodingData): AddressData => addressSchema.parse(data.address),
     staleTime: 3 * 60 * 60 * 1000,
   })
